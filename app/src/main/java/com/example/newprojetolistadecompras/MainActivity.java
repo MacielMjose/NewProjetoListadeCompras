04d1e9cb02309727f6609621b889cbc262ca19ce
MacielMjose <jlmm92.maciel@gmail.com>
2019-04-13 23:45:54 -0300

    Criação activity produtos

--- app/src/main/java/com/example/newprojetolistadecompras/MainActivity.java
+++ app/src/main/java/com/example/newprojetolistadecompras/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.newprojetolistadecompras;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addLista = findViewById(R.id.floatingActionButton2);
     }
 }
