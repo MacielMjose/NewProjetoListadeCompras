d4f55cca79d1b960c7f511ccff598a1bcee4d72f
MacielMjose <jlmm92.maciel@gmail.com>
2019-04-14 18:34:24 -0300

    Fix Activity Add produto, exclusão Intent Desnecessária

--- app/src/main/java/com/example/newprojetolistadecompras/MainActivity.java
+++ app/src/main/java/com/example/newprojetolistadecompras/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.newprojetolistadecompras;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private FloatingActionButton addLista;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addLista = findViewById(R.id.addLista);
+        addLista = (FloatingActionButton) findViewById(R.id.addLista);
+
+        addLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this , AddprodutoActivity.class));
+
+            }
+        });
+
+
     }
 }
