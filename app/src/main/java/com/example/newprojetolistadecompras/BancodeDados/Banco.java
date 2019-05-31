99d2f36050c323fd2fadeb69518d30e6c887bcca
MacielMjose <jlmm92.maciel@gmail.com>
2019-05-30 23:16:30 -0300

    Primeiro Commit, subida pra Branch Revisada

---
+++ app/src/main/java/com/example/newprojetolistadecompras/BancodeDados/Banco.java
@@ -0,0 +1,31 @@
+package com.example.newprojetolistadecompras.BancodeDados;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Banco extends SQLiteOpenHelper {
+
+    private static final String NOME_BANCO = "ListaProdutos";
+    private static final int VERSAO_BANCO = 2;
+
+    public Banco(Context context){
+        super(context, NOME_BANCO, null, VERSAO_BANCO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS lista ( " +
+                "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , " +
+                "  Qtde INTEGER, " +
+                "  NomeDaLista TEXT,"+
+                "  Preco DOUBLE,"+ //mudar para double
+                "  NomeProduto TEXT ) " );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE lista");
+            onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/newprojetolistadecompras/BancodeDados/DatabaseEsquema.java
@@ -0,0 +1,27 @@
+package com.example.newprojetolistadecompras.BancodeDados;
+
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class DatabaseEsquema {      //superclasse que contem os metodos para criar Banco, atualizar, e destruir
+
+    public class CriaBanco extends DatabaseEsquema
+    {
+        //@Override
+
+        public void onCreate(SQLiteDatabase db)
+        {
+
+        }
+    }
+        //@Override
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+
+
+        }
+
+
+}
+
